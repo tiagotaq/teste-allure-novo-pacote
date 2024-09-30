ac94a7fee23c618859606f6222dec3f570e620d3
tiagotaq <101513606+tiagotaq@users.noreply.github.com>
2024-09-30 11:55:31 -0300

    Alterações do Allure

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,13 @@
 const { defineConfig } = require("cypress");
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
   viewportHeight: 880,
   viewportWidth: 1280,
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
+      allureWriter(on, config);
+      return config;
+    }
+  }
 });

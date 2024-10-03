3fe79098433cd46cc9a51a4c4baca298822cc1a7
tiagotaq <101513606+tiagotaq@users.noreply.github.com>
2024-10-03 18:10:24 -0300

    Modificando teste para forçar um erro

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -6,7 +6,7 @@ describe('Central de Atendimento ao Cliente TAT', function () {
   })
 
     it('verifica o título da aplicação', function () {
-      cy.title().should('be.equals', 'Central de Atendimento ao Cliente TAT')
+      cy.title().should('be.equals', 'Central de Atendimento ao Cliente TAT editado')
     })
 
   it('preenche os campos obrigatórios e envia o formulário', function () {
